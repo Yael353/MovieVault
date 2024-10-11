3fb7200a934267dfece4dc33f0d8e0b73c9dbdc4
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2024-10-12 00:25:42 +0200

    Design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import MoviePage from "./pages/MoviePage";
 import FavoritesPage from "./pages/FavoritesPage";
+import "./styles/App.css";
 
 function App() {
   return (
