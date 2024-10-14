f7614d9aeb1e783141b9f93807aa3b774d441019
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2024-10-14 09:47:13 +0200

    test branch navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Navbar() {
+  return <div className="w-full bg-slate-600">Test</div>;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "../components/searchBar.jsx";
 import MoviesList from "../components/MoviesList";
 import "../styles/App.css";
+import Navbar from "../components/Navbar.jsx";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <h1 className="flex justify-center items-center retro-title text-4xl font-bold mb-4 shadow-md shadow-[#22cf22] pb-3">
         MovieVault
       </h1>
+      <Navbar />
       <SearchBar />
       <MoviesList />
     </div>
